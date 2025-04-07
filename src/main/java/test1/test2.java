54abde44fe79fa1c00149e857ae0ab21ae1828e6
Murali <murali.krishnabs@gmail.com>
2025-04-07 12:08:35 +0530

    Update test2.java

--- src/main/java/test1/test2.java
+++ src/main/java/test1/test2.java
@@ -5,7 +5,7 @@ public class test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-System.out.println("Test for puth123");
+System.out.println("Test for puth1233");
 		System.out.println("Test for push123");
 		
 	}
